d021df5e53ceb63d0e1ae8a7660df06b444edb7a
Krishna Vamsi <nvgkvamsi@gmail.com>
2022-06-20 10:17:19 -0700

    Updating roboshop-shell-script

---
+++ components/dispatch.sh
@@ -0,0 +1,18 @@
+yum install golang -y
+
+useradd roboshop
+
+curl -L -s -o /tmp/dispatch.zip https://github.com/roboshop-devops-project/dispatch/archive/refs/heads/main.zip
+unzip -o /tmp/dispatch.zip
+mv dispatch-main dispatch
+cd dispatch
+go mod init dispatch
+go get
+go build
+
+sed -i -e 's/RABBITMQ-IP/rabbitmq.roboshop.internal/' /home/roboshop/dispatch/systemd.service
+
+mv /home/roboshop/dispatch/systemd.service /etc/systemd/system/dispatch.service
+systemctl daemon-reload
+systemctl enable dispatch
+systemctl restart dispatch
