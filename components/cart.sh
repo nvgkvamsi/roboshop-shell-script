c204467d918dd4b5f973a131386dc2660b7eb4c1
Krishna Vamsi <nvgkvamsi@gmail.com>
2022-06-21 16:05:28 -0700

    Updating roboshop-shell-script

--- components/cart.sh
+++ components/cart.sh
@@ -5,27 +5,41 @@ CHECK_ROOT
 
 echo " Setting UP NodeJS YUM REPO is failure"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${LOG}
-CHECk_STAT $?
+CHECK_STAT $?
 
-yum install nodejs -y
+echo " Installing NodeJS"
+yum install nodejs -y &>>${LOG}
+CHECK_STAT $?
 
-useradd roboshop
+echo "Creating Application User"
+useradd roboshop &>>${LOG}
+CHECK_STAT $?
 
-curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip"
+echo "Downloading Cart Content"
+curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip" &>>${LOG}
+CHECK_STAT $?
+
 
 cd /home/roboshop
 
-unzip -o /tmp/cart.zip
+echo "Extract Cart Content"
+unzip -o /tmp/cart.zip &>>${LOG}
+CHECK_STAT $?
 
 mv cart-main cart
-
 cd cart
 
-npm install
+echo "Install NodeJS Dependencies"
+npm install &>>${LOG}
+CHECK_STAT $?
 
-sed -i -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' /home/roboshop/cart/systemd.service
+echo "Update SystemD Configuration"
+sed -i -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' /home/roboshop/cart/systemd.service &>>${LOG}
+CHECK_STAT $?
 
-mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service
+echo "Setup Systemd Service"
+mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service &>>${LOG}
+CHECK_STAT $?
 
 systemctl daemon-reload
 
