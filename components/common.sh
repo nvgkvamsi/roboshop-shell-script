6161587af842789a38ec7ab0c5f1ed6ebb3294ed
Krishna Vamsi <nvgkvamsi@gmail.com>
2022-06-21 15:27:12 -0700

    Updating roboshop-shell-script

--- components/common.sh
+++ components/common.sh
@@ -2,7 +2,7 @@ CHECK_ROOT() {
   USER_ID=$(id -u)
   if [ $USER_ID -ne 0 ]; then
       echo You are not a root user
-      echo -e "\e[You should run this script as a root user or with sudo\["
+      echo -e "\e[33You should run this script as a root user or with sudo\["
       exit 1
   fi
 }
