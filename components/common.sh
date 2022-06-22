fdff447bce2baedd6ca8f4eeb893cd425c332dc2
Krishna Vamsi <nvgkvamsi@gmail.com>
2022-06-22 15:09:52 -0700

    Updating roboshop-shell-script

--- components/common.sh
+++ components/common.sh
@@ -134,7 +134,7 @@ MAVEN() {
   APP_COMMON_SETUP
 
   PRINT "Compile ${COMPONENT} Code"
-  mv ${COMPONENT}-main ${COMPONENT} && cd ${COMPONENT} && mvn clean package && mv target/${COMPONENT}-1.0.jar ${COMPONENT}.jar
+  mv ${COMPONENT}-main ${COMPONENT} && cd ${COMPONENT} && mvn clean package && mv target/${COMPONENT}-1.0.jar ${COMPONENT}.jar &>>${LOG}
   CHECK_STAT $?
 
   SYSTEMD
