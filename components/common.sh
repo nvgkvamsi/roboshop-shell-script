99f8cc427dfe8a8268bd709fb88e4554d38b251e
Krishna Vamsi <nvgkvamsi@gmail.com>
2022-06-22 12:30:10 -0700

    Updating roboshop-shell-script

--- components/common.sh
+++ components/common.sh
@@ -26,3 +26,59 @@ PRINT() {
   echo "$1"
 }
 
+NODEJS()
+{
+  CHECK_ROOT
+
+  PRINT "Setting Up NodeJS YUM REPO"
+  curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${LOG}
+  CHECK_STAT $?
+
+  PRINT "Installing NodeJS"
+  yum install nodejs -y &>>${LOG}
+  CHECK_STAT $?
+
+  PRINT "Creating Application User"
+  id roboshop &>>${LOG}
+  if [ $? -ne 0 ]; then
+    useradd roboshop &>>${LOG}
+  fi
+  CHECK_STAT $?
+
+  PRINT "Downloading ${COMPONENT} Content"
+  curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/roboshop-devops-project/${COMPONENT}/archive/main.zip" &>>${LOG}
+  CHECK_STAT $?
+
+  cd /home/roboshop
+
+  PRINT "Remove Old Content"
+  rm -rf ${COMPONENT} &>>${LOG}
+  CHECK_STAT $?
+
+  PRINT "Extract ${COMPONENT} Content"
+  unzip -o /tmp/${COMPONENT}.zip &>>${LOG}
+  CHECK_STAT $?
+
+  mv ${COMPONENT}-main ${COMPONENT}
+  cd ${COMPONENT}
+
+  PRINT "Install NodeJS Dependencies"
+  npm install &>>${LOG}
+  CHECK_STAT $?
+
+  PRINT "Update SystemD Configuration"
+  sed -i -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' /home/roboshop/${COMPONENT}/systemd.service &>>${LOG}
+  CHECK_STAT $?
+
+  PRINT "Setup Systemd Service"
+  mv /home/roboshop/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service &>>${LOG}
+  CHECK_STAT $?
+
+  systemctl daemon-reload
+  systemctl enable ${COMPONENT}
+
+  PRINT "Start Cart Service"
+  systemctl restart ${COMPONENT} &>>${LOG}
+  CHECK_STAT $?
+
+}
